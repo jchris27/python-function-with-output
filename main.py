120d56e6856296e15039a2bb7a6ce95255a77d7f
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-31 09:33:49 +0000

    added an if statement and early return

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Functions with Outputs
 
 def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
   return f_name.title(), l_name.title()
 
 
-print(format_name("jino", "chris"))
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
